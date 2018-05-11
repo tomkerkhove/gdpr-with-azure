a002c8daedb17e158befef1012cc7500758bb7bb
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-05-11 14:00:41 +0200

    Add WIP script to consolidate data
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Themis.Analytics.Usql/FilterUsersOnDisplayName.usql.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackExchange.Analytics.USql
+{
+    public static class DataCleansing
+    {
+        public static string CleanseString(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            input = input.Trim();
+            input = input.ToLowerInvariant();
+
+            return input;
+        }
+    }
+}
