bb6af1f55b2523b1bd4ca8739a255cecff12d28b
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-05-26 09:40:50 +0200

    Trigger process over APIM
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Themis.Services.Portal/Program.cs
+++ src/Themis.Services.Portal/Program.cs
@@ -1,22 +1,13 @@
-﻿using System.IO;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 
 namespace Themis.Services.Portal
 {
     public class Program
     {
-        public static IConfiguration Configuration { get; set; }
 
         public static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json");
-
-            Configuration = builder.Build();
-
             BuildWebHost(args).Run();
         }
 
